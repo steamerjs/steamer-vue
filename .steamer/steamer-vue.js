0acf6da5c7d6b14faae4954745d9cc0971febb59
lcxfs1991 <lcxfs1991@gmail.com>
2017-02-13 18:40:11 +0800

    improve steamer config and package.json

---
+++ .steamer/steamer-vue.js
@@ -0,0 +1,39 @@
+module.exports = {
+    files: [
+        "src",
+        "tools",
+        "config",
+        "README.md",
+        ".eslintrc.js",
+        ".eslintignore",
+        ".stylelintrc.js",
+        ".gitignore",
+        ".babelrc",
+    ],
+    options: [
+        {
+            type: 'input',
+            name: 'webserver',
+            message: 'html url(//localhost:9000/)',
+            default: "//localhost:9000/",
+        },
+        {
+            type: 'input',
+            name: 'cdn',
+            message: 'cdn url(//localhost:8000/)',
+            default: "//localhost:8000/",
+        },
+        {
+            type: 'input',
+            name: 'port',
+            message: 'development server port(9000)',
+            default: '9000',
+        },
+        {
+            type: 'input',
+            name: 'route',
+            message: 'development server directory(/news/)',
+            default: '/news/',
+        }
+    ]
+};
