0b225983c86fb3e4b494c48634d288991ff5ee76
jeffjing <zgayjjf@qq.com>
2017-03-01 19:22:05 +0800

    删除测试代码

--- src/page/index/main.js
+++ src/page/index/main.js
@@ -5,17 +5,3 @@ new Vue({
     el: '#main',
     render: h => h(Main)
 })
-
-
-var b = new Promise(function (resolve, reject) {
-    setTimeout(function () {
-        resolve(1)
-    })
-})
-b.then(function (number) {
-    console.log(number)
-})
-console.log(1)
-export default {
-    name: '1'
-}
